b7d5e6d55d1f00bf517b3bded875750f304a5f77
JMcB#7918 <joel.mcbride1@live.com>
2020-12-05 20:39:43 +0000

    Get the loser as well as winner.

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ import praw
 # import prawcore
 
 
-# TODO: ban the loser as well as the winner
 # TODO: add functionality to add the winner to the 'Hall of banned users'
 
 
@@ -37,17 +36,29 @@ while True:
         # Get all top posts for the day
         posts_today = list(subreddit.top(time_filter='day'))
         # Sort them by score
+        # Currently, they're sorted by score - the sum of upvotes and downvotes.
+        # However, it's probably possible to get exactly how many upvotes and downvotes they had.
         posts_today.sort(key=lambda submission: submission.score, reverse=True)
         top_post = posts_today[0]
+        bottom_post = posts_today[-1]
 
         date = time.strftime('%d/%m/%Y')
 
+        # Ban the top poster for today
         ban_reason_top = f"Most upvoted post of the day {date}"
         subreddit.banned.add(top_post.author.name, ban_reason=ban_reason_top)
 
-        announcement_post_title = f"Today's ({date}) winner!"
+        # Ban the bottom poster for today
+        ban_reason_bottom = f"Least upvoted post of the day {date}"
+        subreddit.banned.add(bottom_post.author.name, ban_reason=ban_reason_bottom)
+
+        announcement_post_title = f"Today's ({date}) winner and loser!"
         announcement_post_body = f"""u/{top_post.author.name} is our unfortunate [winner]({top_post.permalink})!    
+u/{bottom_post.author.name} is our equally as unfortunate [loser]({bottom_post.permalink})!    
 Keep the posts coming fellas, you could be added to our hall of winners and losers if you’re (un)lucky enough!"""
         subreddit.submit(title=announcement_post_title,
                          selftext=announcement_post_body,
                          flair_id=ANNOUNCEMENT_FLAIR_ID)
+
+        # Ensure no double dipping
+        time.sleep(2)
